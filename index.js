ae03b1cbb8b6f243003d170992b9bd114182d565
piasmajumdar <piasmajumdar@gmail.com>
2026-05-16 14:30:18 +0530

    added middlewar

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 //middlewar
+app.use(cors())
+app.use(express.json())
 
 
 app.get('/', (req, res)=>{
